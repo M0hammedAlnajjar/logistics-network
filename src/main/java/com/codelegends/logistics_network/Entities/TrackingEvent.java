8c75c414c467501d664eb12aefbe6d7f3426be0e
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:20:18 +0400

    feat: create TrackingEvent entity

---
+++ src/main/java/com/codelegends/logistics_network/Entities/TrackingEvent.java
@@ -0,0 +1,33 @@
+package com.codelegends.logistics_network.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+public class TrackingEvent extends BaseClass {
+
+    @Column(nullable = false)
+    private LocalDateTime eventTime;
+
+    @Column(nullable = false, length = 150)
+    private String location;
+
+    @Column(nullable = false, length = 50)
+    private String status;
+
+    @Column(length = 500)
+    private String note;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "shipment_id", nullable = false)
+    private Shipment shipment;
+}
