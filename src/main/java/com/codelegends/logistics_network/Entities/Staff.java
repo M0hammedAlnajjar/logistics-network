ce50ae09e61fc9a07b380a039bd923afee74810a
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:20:20 +0400

    feat: create Staff entity

---
+++ src/main/java/com/codelegends/logistics_network/Entities/Staff.java
@@ -0,0 +1,28 @@
+package com.codelegends.logistics_network.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Staff extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, length = 50)
+    private String role;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouse;
+}
