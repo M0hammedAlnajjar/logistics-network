43a1fe718e80f4ee25d09420c598569980b2fb6c
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 14:50:19 +0400

    refactor: use enums in Vehicle entity

--- src/main/java/com/codelegends/logistics_network/Entities/Vehicle.java
+++ src/main/java/com/codelegends/logistics_network/Entities/Vehicle.java
@@ -1,8 +1,13 @@
 package com.codelegends.logistics_network.Entities;
 
+import com.codelegends.logistics_network.enums.VehicleStatus;
+import com.codelegends.logistics_network.enums.VehicleType;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -22,14 +27,16 @@ public class Vehicle extends BaseClass {
     @Column(nullable = false, unique = true, length = 30)
     private String plateNumber;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 50)
-    private String type;
+    private VehicleType type;
 
     @Column(nullable = false, precision = 12, scale = 2)
     private BigDecimal capacityKg;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 50)
-    private String status;
+    private VehicleStatus status;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "carrier_id", nullable = false)
