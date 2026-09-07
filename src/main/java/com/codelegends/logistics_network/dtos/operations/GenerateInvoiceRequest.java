3fb601ecfe67ab5409c4b5ae98465ddec9fc6dd5
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 14:35:44 +0400

    feat: validate GenerateInvoiceRequest

--- src/main/java/com/codelegends/logistics_network/dtos/operations/GenerateInvoiceRequest.java
+++ src/main/java/com/codelegends/logistics_network/dtos/operations/GenerateInvoiceRequest.java
@@ -1,5 +1,8 @@
 package com.codelegends.logistics_network.dtos.operations;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +17,11 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class GenerateInvoiceRequest {
 
+    @NotNull(message = "Invoice amount is required")
+    @Positive(message = "Invoice amount must be greater than zero")
     private BigDecimal amount;
+
+    @NotNull(message = "Invoice issued date is required")
+    @PastOrPresent(message = "Invoice issued date cannot be in the future")
     private LocalDate issuedDate;
 }
