bbb6c33bf27dbfdaa2afe5b398ce4c0b6e49f3e7
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 10:49:24 +0400

    feat: create Invoice repository

---
+++ src/main/java/com/codelegends/logistics_network/repositories/InvoiceRepository.java
@@ -0,0 +1,16 @@
+package com.codelegends.logistics_network.repositories;
+
+import com.codelegends.logistics_network.Entities.Invoice;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    List<Invoice> findAllByIsActiveTrue();
+
+    Optional<Invoice> findByIdAndIsActiveTrue(Long id);
+}
