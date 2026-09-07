5e713880ea8be1b9c43afeae11be1a466fee5c41
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:19:51 +0400

    feat: create Driver entity

---
+++ src/main/java/com/codelegends/logistics_network/Entities/Driver.java
@@ -0,0 +1,40 @@
+package com.codelegends.logistics_network.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Driver extends BaseClass {
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String licenseNumber;
+
+    @Column(nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(nullable = false, length = 50)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "carrier_id", nullable = false)
+    private Carrier carrier;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "driver")
+    private List<Route> routes = new ArrayList<>();
+}
