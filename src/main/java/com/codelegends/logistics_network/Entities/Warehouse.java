0261a9ff05ce73cf199daf2b55e2cc1c4197b762
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 08:59:24 +0400

    feat: complete Warehouse entity relationships

--- src/main/java/com/codelegends/logistics_network/Entities/Warehouse.java
+++ src/main/java/com/codelegends/logistics_network/Entities/Warehouse.java
@@ -1,8 +1,17 @@
 package com.codelegends.logistics_network.Entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
 public class Warehouse extends BaseClass {
 
@@ -14,4 +23,16 @@ public class Warehouse extends BaseClass {
 
     @Column(nullable = false)
     private Integer capacity;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "warehouse")
+    private List<InventoryItem> inventoryItems = new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "warehouse")
+    private List<Shipment> shipments = new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "warehouse")
+    private List<Staff> staff = new ArrayList<>();
 }
