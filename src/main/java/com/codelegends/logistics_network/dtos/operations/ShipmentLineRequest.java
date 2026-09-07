ed62c67d52c50a256aba43de10be11c34c84df2d
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 14:35:35 +0400

    feat: validate ShipmentLineRequest

--- src/main/java/com/codelegends/logistics_network/dtos/operations/ShipmentLineRequest.java
+++ src/main/java/com/codelegends/logistics_network/dtos/operations/ShipmentLineRequest.java
@@ -1,5 +1,7 @@
 package com.codelegends.logistics_network.dtos.operations;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class ShipmentLineRequest {
 
+    @NotNull(message = "Product ID is required")
+    @Positive(message = "Product ID must be greater than zero")
     private Long productId;
+
+    @NotNull(message = "Item quantity is required")
+    @Positive(message = "Item quantity must be greater than zero")
     private Integer quantity;
 }
