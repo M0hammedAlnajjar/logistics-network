6d011795df02fd93c814d60d35103fd2e39f84d3
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:19:15 +0400

    feat: create InventoryItem entity

---
+++ src/main/java/com/codelegends/logistics_network/Entities/InventoryItem.java
@@ -0,0 +1,29 @@
+package com.codelegends.logistics_network.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class InventoryItem extends BaseClass {
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false, length = 50)
+    private String shelfLocation;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "warehouse_id", nullable = false)
+    private Warehouse warehouse;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+}
