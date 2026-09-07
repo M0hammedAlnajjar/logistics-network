db7eb6eae059069ccfb16c29126e365512faaa47
mohammed <mooodo150@gmail.com>
2026-09-07 08:37:24 +0400

    chore: initialize logistics network Spring Boot project

---
+++ src/main/java/com/codelegends/logistics_network/LogisticsNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.codelegends.logistics_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsNetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codelegends/logistics_network/LogisticsNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelegends.logistics_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
