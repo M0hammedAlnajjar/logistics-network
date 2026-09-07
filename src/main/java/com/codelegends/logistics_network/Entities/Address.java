c0e72fdd58b84f67fbd6824b6926d401eb803ca1
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:19:17 +0400

    feat: create Address entity

---
+++ src/main/java/com/codelegends/logistics_network/Entities/Address.java
@@ -0,0 +1,35 @@
+package com.codelegends.logistics_network.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Address extends BaseClass {
+
+    @Column(nullable = false, length = 200)
+    private String street;
+
+    @Column(nullable = false, length = 100)
+    private String city;
+
+    @Column(nullable = false, length = 20)
+    private String postalCode;
+
+    @Column(nullable = false, length = 100)
+    private String country;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "service_zone_id")
+    private ServiceZone serviceZone;
+}
