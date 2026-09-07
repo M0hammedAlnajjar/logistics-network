7744a8aa624b2bb7ee659d028fa5ce052c9ce1b6
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 14:29:27 +0400

    feat: create ResourceNotFoundException

---
+++ src/main/java/com/codelegends/logistics_network/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.codelegends.logistics_network.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
