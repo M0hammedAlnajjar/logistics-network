2f19b6b40a97bee766f476b22b942e8b38376f88
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 14:50:23 +0400

    refactor: use enums in DeliveryStop entity

--- src/main/java/com/codelegends/logistics_network/Entities/DeliveryStop.java
+++ src/main/java/com/codelegends/logistics_network/Entities/DeliveryStop.java
@@ -1,7 +1,11 @@
 package com.codelegends.logistics_network.Entities;
 
+import com.codelegends.logistics_network.enums.DeliveryStopStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -30,8 +34,9 @@ public class DeliveryStop extends BaseClass {
     @Column(nullable = false, length = 250)
     private String address;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 50)
-    private String status;
+    private DeliveryStopStatus status;
 
     @Column(nullable = false)
     private LocalDateTime eta;
