198859dfc4bb5860e1b758a68a3fe965079f9174
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:19:50 +0400

    feat: create Vehicle entity

---
+++ src/main/java/com/codelegends/logistics_network/Entities/Vehicle.java
@@ -0,0 +1,41 @@
+package com.codelegends.logistics_network.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+public class Vehicle extends BaseClass {
+
+    @Column(nullable = false, unique = true, length = 30)
+    private String plateNumber;
+
+    @Column(nullable = false, length = 50)
+    private String type;
+
+    @Column(nullable = false, precision = 12, scale = 2)
+    private BigDecimal capacityKg;
+
+    @Column(nullable = false, length = 50)
+    private String status;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "carrier_id", nullable = false)
+    private Carrier carrier;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "vehicle")
+    private List<Route> routes = new ArrayList<>();
+}
