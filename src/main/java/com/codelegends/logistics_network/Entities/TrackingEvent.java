7f370fb864fb78eb5fad00356e3f795fe0c8d5e2
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 14:50:24 +0400

    refactor: use enums in TrackingEvent entity

--- src/main/java/com/codelegends/logistics_network/Entities/TrackingEvent.java
+++ src/main/java/com/codelegends/logistics_network/Entities/TrackingEvent.java
@@ -1,7 +1,11 @@
 package com.codelegends.logistics_network.Entities;
 
+import com.codelegends.logistics_network.enums.TrackingStatus;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -21,8 +25,9 @@ public class TrackingEvent extends BaseClass {
     @Column(nullable = false, length = 150)
     private String location;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 50)
-    private String status;
+    private TrackingStatus status;
 
     @Column(length = 500)
     private String note;
