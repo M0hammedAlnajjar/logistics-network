1c26942bdfd296539343f85bb8a58dab779bb683
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 12:33:57 +0400

    feat: add Driver carrier query

--- src/main/java/com/codelegends/logistics_network/repositories/DriverRepository.java
+++ src/main/java/com/codelegends/logistics_network/repositories/DriverRepository.java
@@ -2,6 +2,8 @@ package com.codelegends.logistics_network.repositories;
 
 import com.codelegends.logistics_network.Entities.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,4 +19,11 @@ public interface DriverRepository extends JpaRepository<Driver, Long> {
     boolean existsByLicenseNumber(String licenseNumber);
 
     boolean existsByLicenseNumberAndIdNot(String licenseNumber, Long id);
+
+    @Query("""
+            SELECT COUNT(d) FROM Driver d
+            WHERE d.isActive = true
+              AND d.carrier.id = :carrierId
+            """)
+    long countActiveByCarrierId(@Param("carrierId") Long carrierId);
 }
