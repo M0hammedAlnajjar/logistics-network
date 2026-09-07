e83a6e4274019b48a97bb979ddb707cbb4a88029
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 08:55:27 +0400

    feat: complete BaseClass audit fields

--- src/main/java/com/codelegends/logistics_network/Entities/BaseClass.java
+++ src/main/java/com/codelegends/logistics_network/Entities/BaseClass.java
@@ -1,12 +1,16 @@
 package com.codelegends.logistics_network.Entities;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -16,4 +20,23 @@ public abstract class BaseClass {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private boolean isActive = true;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    public void prePersist() {
+        createdDate = LocalDateTime.now();
+        isActive = true;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
 }
