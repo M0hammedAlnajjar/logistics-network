52f0201adbffbc457ae10a0ac703eacb75fafcb0
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:00:56 +0400

    feat: add name field to Product

--- src/main/java/com/codelegends/logistics_network/Entities/Product.java
+++ src/main/java/com/codelegends/logistics_network/Entities/Product.java
@@ -1,8 +1,11 @@
 package com.codelegends.logistics_network.Entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 
 @Entity
 public class Product extends BaseClass {
 
+    @Column(nullable = false, length = 100)
+    private String name;
 }
