81e53caa6fb2a62d5104d946651c272cb8e3c153
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 14:50:27 +0400

    refactor: use enums in Staff entity

--- src/main/java/com/codelegends/logistics_network/Entities/Staff.java
+++ src/main/java/com/codelegends/logistics_network/Entities/Staff.java
@@ -1,7 +1,11 @@
 package com.codelegends.logistics_network.Entities;
 
+import com.codelegends.logistics_network.enums.StaffRole;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -16,8 +20,9 @@ public class Staff extends BaseClass {
     @Column(nullable = false, length = 100)
     private String name;
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 50)
-    private String role;
+    private StaffRole role;
 
     @Column(nullable = false, length = 20)
     private String phoneNumber;
