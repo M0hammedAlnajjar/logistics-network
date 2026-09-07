77456048462a81620ead58ae3775fbc9e8b0afed
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 08:54:25 +0400

    feat: add Lombok accessors to BaseClass

--- src/main/java/com/codelegends/logistics_network/Entities/BaseClass.java
+++ src/main/java/com/codelegends/logistics_network/Entities/BaseClass.java
@@ -5,9 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @MappedSuperclass
-public class BaseClass {
+public abstract class BaseClass {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
