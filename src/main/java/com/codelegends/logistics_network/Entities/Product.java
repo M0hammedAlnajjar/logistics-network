9a11c48b15c3568f56f09aaecaf068da83fa4469
Mohammed Salim <mooodo150@gmail.com>
2026-09-07 09:01:50 +0400

    feat: add SKU field to Product

--- src/main/java/com/codelegends/logistics_network/Entities/Product.java
+++ src/main/java/com/codelegends/logistics_network/Entities/Product.java
@@ -8,4 +8,7 @@ public class Product extends BaseClass {
 
     @Column(nullable = false, length = 100)
     private String name;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String sku;
 }
